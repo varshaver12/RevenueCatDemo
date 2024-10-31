e644d5c8153917fdffea0266c241a3c07ddc6e9a
varshaver12 <varshaver12@yandex.ru>
2024-10-31 10:25:52 +0700

    Initial Commit

---
+++ RevenueCatDemo/RevenueCatDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RevenueCatDemoApp.swift
+//  RevenueCatDemo
+//
+//  Created by Aleksey on 31.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct RevenueCatDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
